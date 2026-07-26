3f4b5fced248e880187fc293ac287e6684b7a0b4
keananwongso <keananwongso7@gmail.com>
2026-07-26 19:13:46 +0700

    Remove about page and its nav link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const NAV_LINKS = [
-  { href: "/about", label: "about" },
   { href: "/resume", label: "resume" },
 ];
 
